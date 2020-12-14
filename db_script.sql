c0d92b41a2d90da25004d437f74539ecca34dcd7
stephenjonglee <stephenjonglee@csu.fullerton.edu>
2020-12-14 13:55:03 -0800

    added more templates

--- db_script.sql
+++ db_script.sql
@@ -3,39 +3,39 @@
 -- Drop Tables
 DROP TABLE IF EXISTS USER; 
 DROP TABLE IF EXISTS RESTAURANT; 
-DROP TABLE IF EXISTS WAITLIST; 
-DROP TABLE IF EXISTS NAME; 
-DROP TABLE IF EXISTS LOCATION; 
-DROP TABLE IF EXISTS PRICE; 
-DROP TABLE IF EXISTS CATEGORY; 
+DROP TABLE IF EXISTS WAITLIST;
+-- DROP TABLE IF EXISTS NAME; 
+-- DROP TABLE IF EXISTS LOCATION; 
+-- DROP TABLE IF EXISTS PRICE; 
+-- DROP TABLE IF EXISTS CATEGORY; 
 
 -- Create user table
 CREATE TABLE USER (
   user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT unique NOT NULL,
+  email TEXT unique NOT NULL,
   pswd TEXT NOT NULL
 );
 
 -- Create restaurant table
 -- Attributes: ID, name, Location, contact, open, price, category
 CREATE TABLE RESTAURANT (
-    rest_id INTEGER PRIMARY KEY AUTOINCREMENT,
     manager_id int NOT NULL,
-    title varchar(100) NOT NULL,
-	loc varchar(100) NOT NULL,
-	contact varchar(100) NOT NULL,
-	hour varchar(100) NOT NULL,
-    price varchar(100) NOT NULL,
-	category varchar(100) NOT NULL,
+    rest_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+	loc TEXT NOT NULL,
+	contact TEXT NOT NULL,
+	hour TEXT NOT NULL,
+    price TEXT NOT NULL,
+	category TEXT NOT NULL,
     FOREIGN KEY (manager_id) REFERENCES user (id)
 );
 
 -- Create waitlist table
 -- Attributes: id, placement, name, size, time
 CREATE TABLE WAITLIST (
-    cust_id INTEGER PRIMARY KEY AUTOINCREMENT,
     list_id int NOT NULL,
-    cust varchar(100) NOT NULL,
+    cust_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cust TEXT NOT NULL,
     size int NOT NULL,
     start_time time NOT NULL,
     FOREIGN KEY (list_id) REFERENCES RESTAURANT(rest_id)
@@ -45,47 +45,47 @@ CREATE TABLE WAITLIST (
 -- -- Creating Index Tables
 -- -- by name
 -- CREATE TABLE NAME (
---     name varchar(100) NOT NULL PRIMARY KEY,
+--     name TEXT NOT NULL PRIMARY KEY,
 --     rest_id int NOT NULL,
 --     manager_id int NOT NULL,
--- 	location varchar(100) NOT NULL,
--- 	contact varchar(100) NOT NULL,
--- 	open varchar(100) NOT NULL,
---     price varchar(100) NOT NULL,
--- 	category varchar(100) NOT NULL,
+-- 	location TEXT NOT NULL,
+-- 	contact TEXT NOT NULL,
+-- 	open TEXT NOT NULL,
+--     price TEXT NOT NULL,
+-- 	category TEXT NOT NULL,
 --     FOREIGN KEY (manager_id) REFERENCES user (id)
 -- );
 -- -- by Location
 -- CREATE TABLE LOCATION (
---     Location varchar(100) NOT NULL PRIMARY KEY,
+--     Location TEXT NOT NULL PRIMARY KEY,
 --     rest_id int NOT NULL,
---     name varchar(100) NOT NULL,
--- 	contact varchar(100) NOT NULL,
--- 	open varchar(100) NOT NULL,
---     price varchar(100) NOT NULL,
--- 	category varchar(100) NOT NULL,
+--     name TEXT NOT NULL,
+-- 	contact TEXT NOT NULL,
+-- 	open TEXT NOT NULL,
+--     price TEXT NOT NULL,
+-- 	category TEXT NOT NULL,
 --     FOREIGN KEY (manager_id) REFERENCES user (id)
 -- );
 -- -- Search by price
 -- CREATE TABLE PRICE (
---     price varchar(100) NOT NULL PRIMARY KEY,
+--     price TEXT NOT NULL PRIMARY KEY,
 --     rest_id int NOT NULL,
---     name varchar(100) NOT NULL,
--- 	Location varchar(100) NOT NULL,
--- 	contact varchar(100) NOT NULL,
--- 	open varchar(100) NOT NULL,
--- 	category varchar(100) NOT NULL,
+--     name TEXT NOT NULL,
+-- 	Location TEXT NOT NULL,
+-- 	contact TEXT NOT NULL,
+-- 	open TEXT NOT NULL,
+-- 	category TEXT NOT NULL,
 --     FOREIGN KEY (manager_id) REFERENCES user (id)
 -- );
 -- -- by category
 -- CREATE TABLE CATEGORY (
---     category varchar(100) NOT NULL PRIMARY KEY,
+--     category TEXT NOT NULL PRIMARY KEY,
 --     rest_id int NOT NULL,
 
---     name varchar(100) NOT NULL,
--- 	Location varchar(100) NOT NULL,
--- 	contact varchar(100) NOT NULL,
--- 	open varchar(100) NOT NULL,
---     price varchar(100) NOT NULL,
+--     name TEXT NOT NULL,
+-- 	Location TEXT NOT NULL,
+-- 	contact TEXT NOT NULL,
+-- 	open TEXT NOT NULL,
+--     price TEXT NOT NULL,
 --     FOREIGN KEY (manager_id) REFERENCES user (id)
 -- );
--- main.py
+++ main.py
@@ -1,23 +1,79 @@
-import sqlite3
+import flask
+import os
+from flask import *
+import sqlite3, hashlib, os, requests
+from numpy.lib.npyio import save
+from flask import request, url_for, redirect, flash
+from werkzeug.utils import secure_filename
+from flask_login import LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
 
-import click
-from flask import current_app, g
-from flask.cli import with_appcontext
+app = flask.Flask(__name__)
+app.config.from_envvar('APP_CONFIG')
+app.secret_key = app.config['SECRET_KEY']
 
+registration_required = app.config['REGISTER_USER_REQUIRED']
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+login = LoginManager()
 
-    return g.db
+DATABASE_USERS = app.config['DATABASE_USERS']
+DATABASE_CHARACTER = app.config['DATABASE_CHARACTER']
 
+# get database and connect
+def get_db(database):
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(database)
+    return db
 
-def close_db(e=None):
-    db = g.pop('db', None)
+# get login details
+def getLoginDetails():
+    with sqlite3.connect(DATABASE_USERS) as conn:
+        cur = conn.cursor()
+        if 'email' not in session:
+            loggedIn = False
+            username = ''
+        else:
+            loggedIn = True
+            cur.execute("SELECT user_id, username FROM users WHERE email = ?", (session['email'],))
+            user_id, username = cur.fetchone()
+    conn.close()
+    return (loggedIn, user_id, username)
 
-    if db is not None:
-        db.close()
+# validate password
+def is_valid(email, password):
+    database = get_db(DATABASE_USERS)
+    c = database.cursor()
+    c.execute("SELECT EXISTS(SELECT hashed_password FROM users WHERE email=?)", (email,))
+    data = c.fetchall()[0][0]
+    if c.fetchall() == 0:
+        return False
+    c.execute("SELECT hashed_password FROM users WHERE email=?", ([email]))
+    data = c.fetchall()[0][0]
+    if check_password_hash(data, password):
+        return True
+    else:
+        return False
+
+# routes
+@app.route("/")
+def root():
+    return redirect(url_for('index'))
+
+@app.route("/index")
+def index():
+
+    LoggedIn = True
+
+    if 'email' not in session:
+        LoggedIn = False
+
+    return render_template('index.html', LoggedIn=LoggedIn)
+
+@app.route("/logout")
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('welcome'))
+
+if __name__ == '__main__':
+    app.run()
