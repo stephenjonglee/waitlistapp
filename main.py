1f0ea0581c26b0d85324352d6233a3d1f88f997a
stephenjonglee <stephenjonglee@csu.fullerton.edu>
2020-12-14 17:33:43 -0800

    finished htmls

--- main.py
+++ main.py
@@ -1,79 +1,40 @@
-import flask
-import os
 from flask import *
-import sqlite3, hashlib, os, requests
-from numpy.lib.npyio import save
-from flask import request, url_for, redirect, flash
+import sqlite3, hashlib, os
 from werkzeug.utils import secure_filename
-from flask_login import LoginManager
-from werkzeug.security import generate_password_hash, check_password_hash
 
-app = flask.Flask(__name__)
-app.config.from_envvar('APP_CONFIG')
-app.secret_key = app.config['SECRET_KEY']
+app = Flask(__name__)
 
-registration_required = app.config['REGISTER_USER_REQUIRED']
-
-login = LoginManager()
-
-DATABASE_USERS = app.config['DATABASE_USERS']
-DATABASE_CHARACTER = app.config['DATABASE_CHARACTER']
-
-# get database and connect
-def get_db(database):
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(database)
-    return db
-
-# get login details
-def getLoginDetails():
-    with sqlite3.connect(DATABASE_USERS) as conn:
-        cur = conn.cursor()
-        if 'email' not in session:
-            loggedIn = False
-            username = ''
-        else:
-            loggedIn = True
-            cur.execute("SELECT user_id, username FROM users WHERE email = ?", (session['email'],))
-            user_id, username = cur.fetchone()
-    conn.close()
-    return (loggedIn, user_id, username)
-
-# validate password
-def is_valid(email, password):
-    database = get_db(DATABASE_USERS)
-    c = database.cursor()
-    c.execute("SELECT EXISTS(SELECT hashed_password FROM users WHERE email=?)", (email,))
-    data = c.fetchall()[0][0]
-    if c.fetchall() == 0:
-        return False
-    c.execute("SELECT hashed_password FROM users WHERE email=?", ([email]))
-    data = c.fetchall()[0][0]
-    if check_password_hash(data, password):
-        return True
-    else:
-        return False
-
-# routes
-@app.route("/")
+# display root
+@app.route('/')
 def root():
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route("/index")
+# display home page
+@app.route('/index')
 def index():
+    return render_template("index.html")
 
-    LoggedIn = True
+# display login page
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
-    if 'email' not in session:
-        LoggedIn = False
+# display register page
+@app.route('/register')
+def register():
+    return render_template("register.html")
 
-    return render_template('index.html', LoggedIn=LoggedIn)
+# display restaurants
+@app.route('/restaurant')
+def restaurant():
+    return render_template("index.html")
 
-@app.route("/logout")
-def logout():
-    session.pop('email', None)
-    return redirect(url_for('welcome'))
+# display restaurants
+@app.route('/add')
+def add():
+    return render_template("add_rest.html")
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
